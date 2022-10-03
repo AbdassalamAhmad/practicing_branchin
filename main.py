a71eecdd440a3ca77974dfda5413d9c29db87680
AbdassalamAhmad <abdassalam.ahmad98@gmail.com>
2022-10-03 19:06:24 +0200

    add comments

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+# add two numbers
 def adding(x,y):
     return x+y
 
-
+# subtract two numebrs
 def minus(x,y):
     return x-y
 
-
-x = 5
-y = 3
+x= 5
+y= 3
 
 print (adding(x,y))
 print (f"x-y = {minus(x,y)}")
